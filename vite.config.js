6b3d4684887394001c23a6fb6e58d0133338fab5
daiyy <juliette@obamboo.com>
2023-12-21 18:10:35 +0800

    update deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // 開發中、產品路徑
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
+  base:
+    process.env.NODE_ENV === 'production'
+      ? '/frontend-mentor-newsletter/'
+      : '/',
   plugins: [react()],
 });
